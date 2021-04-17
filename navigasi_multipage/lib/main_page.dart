049c6b2dedd37f4abcc3e131eac005ca8397bbdc
Melia Syuhada <meliasyuhada5@gmail.com>
2021-04-17 07:57:56 +0700

    Navigasi Multi Page

---
+++ navigasi_multipage/lib/main_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:navigasi_multipage/second_page.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Main Page"),),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Go to second Page"),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return SecondPage();
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
