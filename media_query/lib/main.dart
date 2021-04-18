ca29ff0adf73a3b0228ffaba68651f5016662692
Melia Syuhada <meliasyuhada5@gmail.com>
2021-04-18 09:54:53 +0700

    Media Query

---
+++ media_query/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Latihan Media Query"),),
+      body: Container(width: MediaQuery.of(context).size.width /3,
+      color: Colors.red,
+      height: MediaQuery.of(context).size.height /2,),
+      );
+  }
+}
